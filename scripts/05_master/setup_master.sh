96f232468c9f7ae38b4a3f16043b6c393324e70a
michaeldbr <31001491+michaeldbr@users.noreply.github.com>
2026-04-10 15:56:22 +0200

    Run Flannel and node readiness checks as michael on master setup

--- scripts/05_master/setup_master.sh
+++ scripts/05_master/setup_master.sh
@@ -9,11 +9,15 @@ else
   echo "kubeadm init voltooid."
 fi
 
+echo "Kubeconfig instellen voor user michael..."
 mkdir -p /home/michael/.kube
 cp /etc/kubernetes/admin.conf /home/michael/.kube/config
 chown -R michael:michael /home/michael/.kube
 
-export KUBECONFIG=/etc/kubernetes/admin.conf
-kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+echo "CNI (Flannel) installeren..."
+su - michael -c "kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml"
 
-echo "Kubeconfig voor michael gezet en Flannel toegepast."
+echo "Wachten tot nodes Ready zijn..."
+su - michael -c "kubectl wait --for=condition=Ready node --all --timeout=120s"
+
+echo "Master setup voltooid: kubeconfig + Flannel + Ready check."
